117711bdf0c1462b30ba480867f404f8281efc75
tothboti <113088585+tothboti@users.noreply.github.com>
2023-10-06 13:19:11 +0200

    Add files via upload

---
+++ feladat/feladat_009.py
@@ -0,0 +1,38 @@
+#feladat_009
+ """
+ kérjuk be ket szamot szam1 szam2
+ hasonlitsuk ossze a ket szamot és irjuk ki hogy a
+ szam1 kisebb mint a szam2 
+ vagy a szam2 kissebb mint a  szam 1
+ vagy a szam1 egyenlo szan2vel 
+ """
+
+szam1 = input("Írj be egy számot /szam1/ : ")
+szam2 = input("Írj be még egy számot /szam2/ : ")
+szam1 = int(szam1)
+szam2 = int(szam2)
+ """
+ if szam1 < szam2:
+    print(f"A szam1 kissebb mint a szam2 ")
+elif szam1 > szam2:
+    print(f"A szam1 nagyobb mint a szam2")
+else:
+    print(f"A szam1 egyenlő a szam2-vel")
+ """
+ 
+ """
+ if szam1 < szam2:
+    print(f"A szam1 kissebb mint a szam2 ")
+if szam1 > szam2:
+    print(f"A szam1 nagyobb mint a szam2")
+if szam1 == szam2:
+    print(f"A szam1 egyenlő a szam2-vel")
+ """
+
+
+if szam1 < szam2:
+    print(f"A szam1 kissebb mint a szam2 ")
+elif szam1 > szam2:
+    print(f"A szam1 nagyobb mint a szam2")
+elif szam1 == szam2:
+    print(f"A szam1 egyenlő a szam2-vel")
