24483813992eb9a2f360081c1d1a3d0ed150cc7e
tothboti <113088585+tothboti@users.noreply.github.com>
2023-10-06 13:03:05 +0200

    Add files via upload

---
+++ feladat/feladat_005.py
@@ -0,0 +1,9 @@
+#feladat_005
+#dk9 104-oldal
+
+felhasználó_kora = 16 
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+felhasználó_kora = str(felhasználó_kora)
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+ilyen = input("És milyen" " "+ felhasználó_kora +" " "évesnek lenni? ")
+olyan = input(f"És milyen {felhasználó_kora} évesnek lenni? ")
---
+++ feladat/feladat_007.py
@@ -0,0 +1,14 @@
+#dk9 108-109 oldal 
+
+
+
+gondolt_szam = 4
+tipp = input("Gondoltam egy számra. Tippeld meg! ")
+tipp = int(tipp)
+if tipp == gondolt_szam:
+    print("Ügyes! ")
+    print("Gratulálok")
+else: 
+    print("Hosszan gondolkodtál rajta?:)")
+    print("Nem érte meg.;)")
+print("Pápá. ")
