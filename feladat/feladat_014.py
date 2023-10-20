7f4610dc0ba67c173cfa59fdfb4c970f58fffd72
tothboti <113088585+tothboti@users.noreply.github.com>
2023-10-20 13:15:51 +0200

    Add files via upload

---
+++ feladat/feladat_014.py
@@ -0,0 +1,16 @@
+# feladat_014
+'''
+Kérjük be a vezeték és keresztnevünket 
+Irassuk ki eljárás segítségével nevunkent
+pl: 
+be:
+'''
+def teljes(vezeteknev2,keresztnev1):
+	     
+            print(f'A teljes neved: {vezeteknev2} {keresztnev1}')
+vezeteknev2 = input('Kérem a vezetékneved: ')
+keresztnev1 = input('Kérem a keresztneved: ')
+
+
+teljes(vezeteknev2,keresztnev1)
+    
---
+++ feladat/proba_20231020.py
@@ -0,0 +1,37 @@
+def koszont():
+	      print('Üdv a fedélzeten!')
+        
+koszont()
+koszont()
+koszont()
+
+#-----------------------------------
+
+def koszont_nevvel(nev):
+	      print('Szia '+ nev +', üdv a fedélzeten!')
+ 
+ 
+koszont_nevvel('Ádám')
+ 
+ 
+ 
+ 
+ #-----------------------------------
+
+
+def koszont_ket_nevvel(nev1, nev2):
+	      #print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+            print(f'Szia {nev1}, {nev2}!\nÜdv a fedélzeten!')
+
+keresztnev1 = "Nóra"
+keresztnev2 = "Ádám"
+
+koszont_ket_nevvel('Nóra', 'Ádám')
+koszont_ket_nevvel(keresztnev1,keresztnev2)
+    
+ 
+ 
+ #-----------------------------------
+
+ 
+ 
