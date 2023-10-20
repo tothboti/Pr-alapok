76da6111e28f4c333be1efd7ee1622e588f295d4
tothboti <113088585+tothboti@users.noreply.github.com>
2023-10-20 13:20:18 +0200

    Add files via upload

--- feladat/feladat_014.py
+++ feladat/feladat_014.py
@@ -5,12 +5,12 @@ Irassuk ki eljárás segítségével nevunkent
 pl: 
 be:
 '''
-def teljes(vezeteknev2,keresztnev1):
-	     
-            print(f'A teljes neved: {vezeteknev2} {keresztnev1}')
+
 vezeteknev2 = input('Kérem a vezetékneved: ')
 keresztnev1 = input('Kérem a keresztneved: ')
 
+def teljes(vnev,knev):
+    print(f'A teljes neved: {vnev} {knev}')
 
 teljes(vezeteknev2,keresztnev1)
-    
+            
