ba0c7210c5b8db64a2b6620cc0d19769da8b62f4
tothboti <113088585+tothboti@users.noreply.github.com>
2023-11-10 13:26:43 +0100

    Add files via upload

---
+++ feladat/feladat_018.py
@@ -0,0 +1,8 @@
+# feladat_018
+#Adatbekérés while-ciklussal (adatkérés megszakítása ENTER-rel)
+
+szo = None
+while szo != '':
+    szo = input('Adj meg szavakat! Ha kilépnél, a szó helyett csak egy ENTER-t üss! ')    
+  
+print('Program vége!')  
