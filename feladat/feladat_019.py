18cf4d3847ddb52f1ca4c9e7af473710fec5a1eb
tothboti <113088585+tothboti@users.noreply.github.com>
2023-11-17 13:19:27 +0100

    Add files via upload

---
+++ feladat/feladat_019.py
@@ -0,0 +1,10 @@
+#feladat_019
+
+sor = 1 
+while sor <= 4:
+    oszlop = 1
+    while oszlop <= 7:
+        print('O ',end='')
+        oszlop = oszlop + 1
+    print('')
+    sor = sor + 1 
---
+++ feladat/feladat_020.py
@@ -0,0 +1,5 @@
+# kiirast kovetoen a kurzor a kiiras vegen marad 
+
+print('teszt', end='')
+print('teszt', end='')
+print('teszt', end='')
