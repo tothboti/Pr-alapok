5412c287350a7a825a11468cc2e52f8778c20fed
tothboti <113088585+tothboti@users.noreply.github.com>
2023-12-01 13:28:34 +0100

    Add files via upload

--- feladat/feladat_026.py
+++ feladat/feladat_026.py
@@ -1,12 +1,12 @@
 # 0 -> 9
 for i in range(10):
-    print(i)
-
+    print(i, end=" ")
+print()
 # 5 -> 8
 for i in range(5,9):
-    print(i)
-
+    print(i, end=" ")
+print()
 # 3 -> 20-ig 6-osával
 for i in range(3,21,6):
-    print(i)
+    print(i, end=" ")
  
